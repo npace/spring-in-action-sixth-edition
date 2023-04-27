a842990f5e00c15dbeb474683132d1dcde1a01c2
npace <lubonay@gmail.com>
2023-04-27 22:47:25 +0300

    Replace HomeController with a simple ViewController config

--- src/test/kotlin/com/example/tacocloud/HomePageTest.kt
+++ src/test/kotlin/com/example/tacocloud/HomePageTest.kt
@@ -8,8 +8,8 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
-@WebMvcTest(HomeController::class)
-class HomeControllerTest {
+@WebMvcTest
+class HomePageTest {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
