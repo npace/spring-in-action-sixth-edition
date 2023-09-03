a80d6af97a122f4b810e24c72c0127e9bd09ab00
npace <lubonay@gmail.com>
2023-09-03 16:13:51 +0300

    Replace data.sql with config

--- src/main/kotlin/com/example/tacocloud/TacoCloudApplication.kt
+++ src/main/kotlin/com/example/tacocloud/TacoCloudApplication.kt
@@ -1,11 +1,34 @@
 package com.example.tacocloud
 
+import com.example.tacocloud.Ingredient.Type
+import com.example.tacocloud.data.IngredientRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 class TacoCloudApplication
 
 fun main(args: Array<String>) {
-	runApplication<TacoCloudApplication>(*args)
+    runApplication<TacoCloudApplication>(*args)
+}
+
+@Configuration
+class Config {
+
+    @Bean
+    fun dataLoader(repo: IngredientRepository): CommandLineRunner = CommandLineRunner {
+        repo.save(Ingredient("FLTO", "Flour Tortilla", Type.Wrap))
+        repo.save(Ingredient("COTO", "Corn Tortilla", Type.Wrap))
+        repo.save(Ingredient("GRBF", "Ground Beef", Type.Protein))
+        repo.save(Ingredient("CARN", "Carnitas", Type.Protein))
+        repo.save(Ingredient("TMTO", "Diced Tomatoes", Type.Veggies))
+        repo.save(Ingredient("LETC", "Lettuce", Type.Veggies))
+        repo.save(Ingredient("CHED", "Cheddar", Type.Cheese))
+        repo.save(Ingredient("JACK", "Monterrey Jack", Type.Cheese))
+        repo.save(Ingredient("SLSA", "Salsa", Type.Sauce))
+        repo.save(Ingredient("SRCR", "Sour Cream", Type.Sauce))
+    }
 }
