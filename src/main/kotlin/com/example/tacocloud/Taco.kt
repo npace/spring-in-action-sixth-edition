1455fe6d55f2cf6afa8fcd716d25bb204e0e46be
npace <lubonay@gmail.com>
2023-09-03 15:33:26 +0300

    Fix Taco

--- src/main/kotlin/com/example/tacocloud/Taco.kt
+++ src/main/kotlin/com/example/tacocloud/Taco.kt
@@ -6,8 +6,12 @@ import java.util.*
 data class Taco(
     var id: Long = 0,
     var createdAd: Date = Date(),
+
+    var createdAt: Date = Date(),
+
     @field:Size(min = 5, message = "Name must be at least 5 characters long")
     var name: String = "",
+
     @field:Size(min = 1, message = "You must choose at least 1 ingredient")
     var ingredients: List<Ingredient> = emptyList()
 )
