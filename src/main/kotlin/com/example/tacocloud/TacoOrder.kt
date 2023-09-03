41bfb08a53bba80779acd30e6f3615f3a45337e9
npace <lubonay@gmail.com>
2023-09-03 16:40:03 +0300

    Fix CC_CVV column name

--- src/main/kotlin/com/example/tacocloud/TacoOrder.kt
+++ src/main/kotlin/com/example/tacocloud/TacoOrder.kt
@@ -1,6 +1,7 @@
 package com.example.tacocloud
 
 import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -45,6 +46,7 @@ data class TacoOrder(
     var ccExpiration: String? = null,
 
     @field:Digits(integer = 3, fraction = 0, message = "Invalid CVV")
+    @Column(name = "CC_CVV")
     var ccCVV: String? = null,
 
     @OneToMany(cascade = [CascadeType.ALL])
